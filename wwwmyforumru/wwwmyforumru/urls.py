77ef5d7f49f4cb704bc59c514d0c8821ed0142e5
neseleznev <ne.seleznev@gmail.com>
2014-12-16 21:13:42 +0500

    temp commit

--- wwwmyforumru/wwwmyforumru/urls.py
+++ wwwmyforumru/wwwmyforumru/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.conf import settings
@@ -9,11 +10,11 @@ urlpatterns = patterns(
     url(r'^accounts/login/$', auth_views.login, name='login'),
     url(r'^accounts/logout/$', auth_views.logout, dict(next_page='/')),
     url(r'', include('forum.urls', namespace='forum')),
-)
+)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += patterns(
-        'django.views.static',
-        (r'media/(?P<path>.*)',
-         'serve',
-         {'document_root': settings.MEDIA_ROOT}), )
+        '',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True})
+    )
